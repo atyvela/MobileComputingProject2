111381235d9bd628706beba451a0fd1807510e9c
atyvela <32454409+acp-proju@users.noreply.github.com>
2020-01-27 20:42:22 +0200

    HW2 update

--- app/src/main/java/com/example/mobilecomputingproject/MainActivity.kt
+++ app/src/main/java/com/example/mobilecomputingproject/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.example.mobilecomputingproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var fabOpened = false
+
+        button_time.setOnClickListener{
+            startActivity(Intent(applicationContext, TimeActivity::class.java))
+        }
+
+        button_map.setOnClickListener{
+            startActivity(Intent(applicationContext, MapActivity::class.java))
+        }
+
+
+
+        fab.setOnClickListener{
+            if(!fabOpened){
+                fabOpened = true
+                fab_map.animate().translationY(-resources.getDimension(R.dimen.standard_66))
+                fab_time.animate().translationY(-resources.getDimension(R.dimen.standard_116))
+            }else{
+                fabOpened = false
+                fab_map.animate().translationY(0f)
+                fab_time.animate().translationY(0f)
+            }
+        }
+
+        fab_time.setOnClickListener{
+            startActivity(Intent(applicationContext, TimeActivity::class.java))
+        }
+
+        fab_map.setOnClickListener{
+            startActivity(Intent(applicationContext, MapActivity::class.java))
+        }
+
     }
 }
+
