fd834ba3d9f6d5ee70a20e6fa4feeed784c99731
atyvela <32454456+atyvela@users.noreply.github.com>
2020-03-10 09:43:41 +0200

    Last HW Update

--- app/src/main/java/com/example/mobilecomputingproject/AppDatabase.kt
+++ app/src/main/java/com/example/mobilecomputingproject/AppDatabase.kt
@@ -4,6 +4,6 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [Reminder::class], version = 1)
-abstract  class AppDatabase : RoomDatabase(){
-    abstract  fun reminderDao() : ReminderDao
+abstract class AppDatabase : RoomDatabase(){
+    abstract fun reminderDao() : ReminderDao
 }
--- app/src/main/java/com/example/mobilecomputingproject/MainActivity.kt
+++ app/src/main/java/com/example/mobilecomputingproject/MainActivity.kt
@@ -1,26 +1,33 @@
 package com.example.mobilecomputingproject
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Slide FAB menu buttons open
         var fabOpened = false
-
-
-
-
-
-        fab.setOnClickListener{
+        fab.setOnClickListener {
             if(!fabOpened){
                 fabOpened = true
                 fab_map.animate().translationY(-resources.getDimension(R.dimen.standard_66))
@@ -33,21 +40,26 @@ class MainActivity : AppCompatActivity() {
         }
 
         fab_time.setOnClickListener{
-            startActivity(Intent(applicationContext, TimeActivity::class.java))
+            val intent = Intent(applicationContext, TimeActivity::class.java)
+            startActivity(intent)
         }
 
-        fab_map.setOnClickListener{
-            startActivity(Intent(applicationContext, MapActivity::class.java))
+        fab_map.setOnClickListener {
+            val intent = Intent(applicationContext, MapActivity::class.java)
+            startActivity(intent)
         }
 
+//        val data = arrayOf("Oulu", "Helsinki", "Tampere")
+//        val reminderAdapter = ReminderAdapter(applicationContext, data)
+//        list.adapter = reminderAdapter
 
-
+        //ex doAsync
     }
 
     override fun onResume() {
         super.onResume()
-        refreshList()
 
+        refreshList()
     }
 
     private fun refreshList(){
@@ -57,13 +69,38 @@ class MainActivity : AppCompatActivity() {
             db.close()
 
             uiThread {
+
                 if(reminders.isNotEmpty()){
                     val adapter = ReminderAdapter(applicationContext, reminders)
                     list.adapter = adapter
-                } else{
-                    toast("No Reminders")
+                }else{
+                    toast("No reminders yet")
                 }
             }
+        }
+    }
+
+    companion object {
+        val CHANNEL_ID="REMINDER_CHANNEL_ID"
+        var NotificationID=1567
+        fun showNotification(context: Context, message:String){
+            var notificationBuilder=NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_search_24px)
+                .setContentTitle("Reminder").setContentText(message)
+                .setStyle(NotificationCompat.BigTextStyle().bigText(message))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            var notificationManager=context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+                val channel=NotificationChannel(
+                    CHANNEL_ID,
+                    context.getString(R.string.app_name),
+                    NotificationManager.IMPORTANCE_DEFAULT)
+                    .apply{description = context.getString(R.string.app_name)}
+                notificationManager.createNotificationChannel(channel)
+            }
+            val notification=NotificationID + Random(NotificationID).nextInt(1, 30)
+            notificationManager.notify(notification, notificationBuilder.build())
         }
     }
 }
